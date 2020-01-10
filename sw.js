a8e78caa7ff4ab778a08831529b32affcb490e8d
LuisLimaDev <56639074+LuisLimaDev@users.noreply.github.com>
2020-01-10 12:22:02 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // CODELAB: Register service worker.
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.getRegistrations().then((r)=>{r[0].unregister()});
+    navigator.serviceWorker.getRegistrations().then(function(registrations) { for(let registration of registrations) { registration.unregister() } })
     navigator.serviceWorker.register('/service-worker.js')
         .then((reg) => {
           console.log('Service worker registered.', reg);
