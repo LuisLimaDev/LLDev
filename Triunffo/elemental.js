dfc517f1673a10e7f807d5976453d1b043c55f35
LuisLimaDev <56639074+LuisLimaDev@users.noreply.github.com>
2021-05-06 11:39:06 -0300

    Update elemental.js

--- Triunffo/elemental.js
+++ Triunffo/elemental.js
@@ -74,3 +74,78 @@ class Elementos {
     }
 
 }
+
+class ElementosSVG {
+    novoSVG( elementoInternoSVG ){
+        svgTag = document.createElementNS( "http://www.w3.org/2000/svg", "svg");
+        svgTag.setAttribute( "xmlns", "http://www.w3.org/2000/svg" );
+        svgTag.setAttribute( "xmlns:xlink", "http://www.w3.org/1999/xlink" );
+        svgTag.append( elementoInternoSVG );
+        return svgTag;
+    }
+
+    novoDefs( itensDeEstiloSVG ){ // CRIA UM ITEM DE ESTILOS DO "SVG"
+        estilosSVG = novoElm("defs");
+        estilosSVG.append( itensDeEstiloSVG );
+        return estilosSVG;
+    }
+
+    novoPattern( linkParaImagem, idDoPattern ){ // ELEMENTO DE PLANO DE FUNDO
+        novoPattern = novoElm("pattern");
+        novoPattern.setAttributeNS("preserveAspectRatio", "none" );
+        novoPattern.setAttributeNS("id", idDoPattern );
+        novoPattern.setAttributeNS( null, "width", "100%" );
+        novoPattern.setAttributeNS( null, "height", "100%" );
+        novoPattern.append( imagemSVG( "imagePattern", linkParaImagem ) );
+        return novoPattern;
+    }
+
+    imagemSVG( idDaImagem, linkDaImagem ){
+        patternImage = novoElm("image");
+        patternImage.setAttributeNS("xlink:href", linkDaImagem);
+        patternImage.setAttributeNS( null, "width", "100%" );
+        patternImage.setAttributeNS( null, "height", "100%" );
+        return patternImage;
+    }
+
+    linearGradient2CoresA( idDaGrade, corInicial, corFinal ){ // CRIA UM PLANO DE FUNDO
+		return '<linearGradient id="' + idDaGrade + '" y1="0.024" x2="1" y2="1" gradientUnits="objectBoundingBox"><stop offset="0" stop-color="' + corInicial + '" ></stop><stop offset="1" stop-color="' + corFinal + '"></stop></linearGradient>';
+	}
+    linearGradient2Cores( idDaGrade, corInicial, corFinal ){ // CRIA UM PLANO DE FUNDO
+        nLinearGradient = novoElm("linearGradient");
+        //nLinearGradient.setAttributeNS( null, "gradientUnits", "objectBoundingBox" );
+        nLinearGradient.setAttributeNS( null, "id", idDaGrade );
+        nLinearGradient.setAttributeNS( null, "x1", "0%" );
+        nLinearGradient.setAttributeNS( null, "x2", "100%" );
+        nLinearGradient.setAttributeNS( null, "y1", "0%" );
+        nLinearGradient.setAttributeNS( null, "y2", "100%" );
+
+        corDeParada1 = novoElm("linearGradient");
+        corDeParada1.setAttributeNS( null, "opacity", "1.0" );
+        corDeParada1.setAttributeNS( null, "offset", "0%" );
+        corDeParada1.setAttributeNS( null, "stop-color", corInicial );
+
+        corDeParada2 = novoElm("linearGradient");
+        corDeParada2.setAttributeNS( null, "opacity", "1.0" );
+        corDeParada2.setAttributeNS( null, "offset", "100%" );
+        corDeParada2.setAttributeNS( null, "stop-color", corFinal );
+
+        nLinearGradient.append( corDeParada1, corDeParada2 );
+        return nLinearGradient;
+    }
+
+    novoGrupo( itensParaAgrupar, idDoGrupo ){
+        agrupamentoSVG = novoElm("g")
+        agrupamentoSVG.id = idDoGrupo;
+        agrupamentoSVG.append( itensParaAgrupar );
+        return agrupamentoSVG;
+    }
+
+    novaPolyforma( idDaForma, desenhoDaForma, estilosDaPolyforma ){
+        return "<polygon id='" + idDaForma + "' points='" + desenhoDaForma + "' style='" + estilosDaPolyforma + "'/>";
+    }
+	
+	novoCirculo( distanciaX, distanciaY, tamanhoDoRadio, identificacao, estilosDoCirculoCSS ){
+		return '<circle id="' + identificacao + '" cx="' + distanciaX + '" cy="' + distanciaY + '" r="' + tamanhoDoRadio + '" style="' + estilosDoCirculoCSS + '" />'
+	}
+}
